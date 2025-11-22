0573419e3392aa7a83b5a096ab1a563e8d2c9d07
ulvialv <ulvialiyevgithub@gmail.com>
2025-11-22 13:02:54 +0000

    asdas

---
+++ python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Square class that defines a square with size, area and printing."""
+
+class Square:
+    """Class that defines a square."""
+
+    def __init__(self, size=0):
+        """Initialize the square with size."""
+        self.size = size   # property setter-i işə salır
+
+    @property
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Print the square with # character."""
+        if self.__size == 0:
+            print("")
+            return
+        for _ in range(self.__size):
+            print("#" * self.__size)
