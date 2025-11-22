f1fa8cd63b67fc630bf79e1a9ac5b1eeb5703a9f
ulvialv <ulvialiyevgithub@gmail.com>
2025-11-22 12:49:15 +0000

    asd

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class.
+"""
+
+
 class Square:
+    """Represents a square."""
     def __init__(self, size):
-        self._size = size
+        self.__size = size
